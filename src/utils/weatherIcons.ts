2466f7c1618bfb0420e6e69d4d5dab65b8049811
Yuka <yuka.ishiwata@invokemedia.com>
2025-12-24 11:33:15 -0800

    refactor: reorganize weather icons and improve icon mappings

--- src/utils/weatherIcons.ts
+++ src/utils/weatherIcons.ts
@@ -6,20 +6,26 @@
  */
 
 // Import all weather icons
-import clearIcon from "@/assets/clear-icon.svg";
-import mostlyCloudyIcon from "@/assets/mostlycloudy-icon.svg";
-import ntChanceFlurriesIcon from "@/assets/nt_chanceflurries-icon.svg";
-import ntChanceRainIcon from "@/assets/nt_chancerain-icon.svg";
-import ntClearIcon from "@/assets/nt_clear-icon.svg";
-import ntCloudyIcon from "@/assets/nt_cloudy-icon.svg";
-import ntFogIcon from "@/assets/nt_fog-icon.svg";
-import ntMostlyCloudyIcon from "@/assets/nt_mostlycloudy-icon.svg";
-import ntPartlyCloudyIcon from "@/assets/nt_partlycloudy-icon.svg";
-import ntRainIcon from "@/assets/nt_rain-icon.svg";
-import ntSleetIcon from "@/assets/nt_sleet-icon.svg";
-import ntTstorms1Icon from "@/assets/nt_tstorms-icon-1.svg";
-import ntTstormsIcon from "@/assets/nt_tstorms-icon.svg";
-import partlySunnyIcon from "@/assets/partlysunny-icon.svg";
+// Day icons (with sun imagery)
+import clearIcon from "@/assets/weather/clear-icon.svg";
+import partlySunnyIcon from "@/assets/weather/partlysunny-icon.svg";
+import mostlyCloudyIcon from "@/assets/weather/mostlycloudy-icon.svg";
+
+// Night-specific icons (with moon imagery)
+import ntClearIcon from "@/assets/weather/nt_clear-icon.svg";
+import ntPartlyCloudyIcon from "@/assets/weather/nt_partlycloudy-icon.svg";
+import ntMostlyCloudyIcon from "@/assets/weather/nt_mostlycloudy-icon.svg";
+
+// Generic weather icons (work for both day and night)
+import chanceFlurriesIcon from "@/assets/weather/chanceflurries-icon.svg";
+import chanceRainIcon from "@/assets/weather/chancerain-icon.svg";
+import chanceSleetIcon from "@/assets/weather/chancesleet-icon.svg";
+import cloudIcon from "@/assets/weather/cloud-icon.svg";
+import fogIcon from "@/assets/weather/fog-icon.svg";
+import rainIcon from "@/assets/weather/rain-icon.svg";
+import sleetIcon from "@/assets/weather/sleet-icon.svg";
+import thunderIcon from "@/assets/weather/thunder-icon.svg";
+import tstormsIcon from "@/assets/weather/tstorms-icon.svg";
 
 export function getWeatherIcon(weatherCode: string): string {
   if (!weatherCode) return partlySunnyIcon;
@@ -27,90 +33,96 @@ export function getWeatherIcon(weatherCode: string): string {
   const code = parseInt(weatherCode, 10);
 
   switch (code) {
-    // Sunny / Clear (day)
+    // Sunny / Clear
     case 1: // Sunny
     case 2: // Mostly Sunny
       return clearIcon;
 
-    // Clear (night)
-    case 33: // Clear (night)
-    case 34: // Mostly Clear (night)
-      return ntClearIcon;
-
-    // Partly Cloudy / Partly Sunny (day)
+    // Partly Cloudy / Partly Sunny
     case 3: // Partly Sunny
     case 4: // Intermittent Clouds
     case 5: // Hazy Sunshine
       return partlySunnyIcon;
 
-    // Partly Cloudy (night)
-    case 35: // Partly Cloudy (night)
-    case 36: // Intermittent Clouds (night)
-    case 37: // Hazy Moonlight (night)
-      return ntPartlyCloudyIcon;
-
-    // Mostly Cloudy / Cloudy (day)
+    // Mostly Cloudy / Cloudy
     case 6: // Mostly Cloudy
     case 7: // Cloudy
     case 8: // Dreary (Overcast)
       return mostlyCloudyIcon;
 
-    // Mostly Cloudy (night)
-    case 38: // Mostly Cloudy (night)
-      return ntMostlyCloudyIcon;
-
     // Fog
     case 11: // Fog
-      return ntFogIcon;
+      return fogIcon;
 
-    // Rain / Showers (day)
+    // Rain / Showers
     case 12: // Showers
     case 13: // Mostly Cloudy w/ Showers
     case 14: // Partly Sunny w/ Showers
+      return chanceRainIcon;
+
     case 18: // Rain
-      return ntChanceRainIcon;
-
-    // Rain / Showers (night)
     case 26: // Freezing Rain
-    case 39: // Partly Cloudy w/ Showers (night)
-    case 40: // Mostly Cloudy w/ Showers (night)
-      return ntRainIcon;
+      return rainIcon;
 
-    // Thunderstorms (day)
+    // Thunderstorms
     case 15: // Thunderstorms
     case 16: // Mostly Cloudy w/ Thunderstorms
     case 17: // Partly Sunny w/ Thunderstorms
-      return ntTstormsIcon;
+      return tstormsIcon;
 
-    // Thunderstorms (night)
-    case 41: // Partly Cloudy w/ Thunderstorms (night)
-    case 42: // Mostly Cloudy w/ Thunderstorms (night)
-      return ntTstorms1Icon;
-
-    // Snow / Flurries (day)
+    // Snow / Flurries
     case 19: // Flurries
     case 20: // Mostly Cloudy w/ Flurries
     case 21: // Partly Sunny w/ Flurries
     case 22: // Snow
     case 23: // Mostly Cloudy w/ Snow
-      return ntChanceFlurriesIcon;
-
-    // Snow / Flurries (night)
-    case 43: // Mostly Cloudy w/ Flurries (night)
-    case 44: // Mostly Cloudy w/ Snow (night)
-      return ntChanceFlurriesIcon;
+      return chanceFlurriesIcon;
 
     // Ice / Sleet
     case 24: // Ice
     case 25: // Sleet
-    case 29: // Rain and Snow
-      return ntSleetIcon;
+      return chanceSleetIcon;
 
-    // Windy / Extreme (using cloudy as placeholder)
+    // Rain and Snow mix
+    case 29: // Rain and Snow
+      return sleetIcon;
+
+    // Windy / Extreme
     case 30: // Hot
     case 31: // Cold
     case 32: // Windy
-      return ntCloudyIcon;
+      return cloudIcon;
+
+    // ==================== NIGHT ====================
+    // Clear
+    case 33: // Clear
+    case 34: // Mostly Clear
+      return ntClearIcon;
+
+    // Partly Cloudy
+    case 35: // Partly Cloudy
+    case 36: // Intermittent Clouds
+    case 37: // Hazy Moonlight
+      return ntPartlyCloudyIcon;
+
+    // Mostly Cloudy / Cloudy
+    case 38: // Mostly Cloudy
+      return ntMostlyCloudyIcon;
+
+    // Rain / Showers
+    case 39: // Partly Cloudy w/ Showers
+    case 40: // Mostly Cloudy w/ Showers
+      return rainIcon;
+
+    // Thunderstorms
+    case 41: // Partly Cloudy w/ Thunderstorms
+    case 42: // Mostly Cloudy w/ Thunderstorms
+      return thunderIcon;
+
+    // Snow / Flurries
+    case 43: // Mostly Cloudy w/ Flurries
+    case 44: // Mostly Cloudy w/ Snow
+      return chanceFlurriesIcon;
 
     // Default fallback for unknown codes
     default:
